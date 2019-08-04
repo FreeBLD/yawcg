d492dbaa5a65e565b8bcb9f88cbdaf4d714134e8
Cristian Pogolsha <cpogolsha@gmail.com>
2019-08-04 20:12:10 +0200

    remove dangling testing template component

--- whatsupdude-element/whatsupdude-element.test.js
+++
@@ -1,7 +0,0 @@
-import  { WhatsupDude } from "./whatsupdude-element";
-
-
-test ("WhatsupDude has a property 'name' with value of 'WhatsupDude'", () => {
-    const WhatsupDude = new WhatsupDude();
-    expect(WhatsupDude.name).toBe('WhatsupDude');
-});
